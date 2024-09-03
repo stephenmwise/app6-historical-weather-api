b53008bc11796a6ba4cfceee309778fc28409b13
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-09-03 13:10:27 -0400

    Add temperature data

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template("home.html")
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date, temperature):
+def about(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {"station": station,
             "date": date,
             "temperature": temperature}
